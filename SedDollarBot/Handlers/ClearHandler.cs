d789caaf62071ecbdcd9e4f7c2cb6bbde5f6e526
YogurtTheHorse <yegorf1@gmail.com>
2020-01-22 00:44:50 +0300

    Restrict /clear command to non-admin users

--- SedDollarBot/Handlers/ClearHandler.cs
+++ SedDollarBot/Handlers/ClearHandler.cs
@@ -1,22 +1,37 @@
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace SedDollarBot.Handlers
 {
     public class ClearHandler : IMessageHandler
     {
-        public bool IsAcceptable(Message message) => message.Text.ToLower() == "/clear";
+        public bool IsAcceptable(Message message) => message.Text.ToLower().StartsWith("/clear");
 
         public async Task Handle(Message message, TelegramBotClient bot)
         {
-            int deleted = DelayedSubstituteHandler.Clear();
+            ChatMember chatMember = await bot.GetChatMemberAsync(message.Chat.Id, message.From.Id);
 
-            await bot.SendTextMessageAsync(
-                message.Chat.Id,
-                $"Deleted {deleted} delayed substitutions.",
-                replyToMessageId: message.MessageId
-            );
+            if (chatMember.Status == ChatMemberStatus.Administrator || chatMember.Status == ChatMemberStatus.Creator)
+            {
+                int deleted = DelayedSubstituteHandler.Clear();
+                
+                await bot.SendTextMessageAsync(
+                    message.Chat.Id,
+                    $"Deleted {deleted} delayed substitutions.",
+                    replyToMessageId: message.MessageId
+                );
+            }
+            else
+            {
+             
+                await bot.SendTextMessageAsync(
+                    message.Chat.Id,
+                    "Only admin can do that..",
+                    replyToMessageId: message.MessageId
+                );   
+            }
         }
     }
 }
